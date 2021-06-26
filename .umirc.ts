efb0a156557e8509f59fc3d50bc74a61e832a9cf
zxf4399 <zxf4399@gmail.com>
2021-06-26 20:28:32 +0800

    feat: locales remove en-US

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
-  // more config: https://d.umijs.org/config
+  locales: [['zh-CN', '中文']],
 });
